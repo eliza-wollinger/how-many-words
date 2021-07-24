fb2c7329f44a2215dc20d17de6c0db32f6c956ea
wollieliza <wollingereliza@gmail.com>
2021-07-24 00:07:59 -0300

    Second commit 🔡

--- contador.py
+++ contador.py
@@ -2,6 +2,6 @@ import sys
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
+    words = sc.textFile("C:\Users\wolli\3D Objects\digital-innovatio-one-data-science\9 second-project\dataproc-challenge/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
+    wordCounts.saveAsTextFile("C:\Users\wolli\3D Objects\digital-innovatio-one-data-science\9 second-project\dataproc-challenge/resultado")
